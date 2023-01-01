4796e0c8440d0637789f1808dd114457e5464dfd
trapperboy <92577580+trapperboy@users.noreply.github.com>
2023-01-01 22:09:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,7 +55,7 @@ HOW_DWLD_LINK = environ.get('HOW_DWLD_LINK', 'https://t.me/moviesbotchannel')
 
 # Log Channels
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', -1001739757555)
-RQST_LOG_CHANNEL = int(environ.get('RQST_LOG_CHANNEL', -1001739757555)
+RQST_LOG_CHANNEL = int(environ.get('RQST_LOG_CHANNEL', -1001739757555))
 
 # Bot Options
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
